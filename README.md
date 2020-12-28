afc3cbacfc8efdfac111dd5d7fd00281daafc35e
xFueY <62284893+xFueY@users.noreply.github.com>
2020-12-28 16:18:37 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Python Auth.GG Package
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+Classifiers = [
+  "Development Status :: 5 - Production/Stable",
+  "Intended Audience :: Developers",
+  "License :: OSI Approved :: MIT License",
+  "Natural Language :: English",
+  "Programming Language :: Python :: 3.8"
+]
+
+setup(
+  name="PyAuthGG",
+  version="1.0.4",
+  description="Simple Python Auth.GG Package",
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url="https://github.com/xFueY/PyAuthGG/",
+  author="xFueY",
+  author_email="xFueY@protonmail.com",
+  license="MIT",
+  classifiers=Classifiers,
+  packages=find_packages(),
+  keywords="AuthGG",
+  install_requires=["requests"]
+)
