c6fab9ea9d2ef80d0080e96a2a6a84ec24910745
xFueY <62284893+xFueY@users.noreply.github.com>
2020-12-28 16:17:37 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from .Administration import *
-from .Application import *
-from .Exceptions import *
-
-Version = "1.0.2"
-Author = "xFueY"
-Source = "https://github.com/xFueY/PyAuthGG"
-
-HEADERS = {"Content-Type" : "application/x-www-form-urlencoded", "User-Agent" : f"PyAuthGG {Version}"}
